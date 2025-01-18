7415e2c6bc25a389e52873af67e57554c73c7e8a
Alican Sekban <alicansekban@hotmail.com>
2025-01-18 18:01:31 +0300

    added Museum screen

---
+++ MuseumsTurkey/screens/MuseumScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MuseumScreen.swift
+//  MuseumsTurkey
+//
+//  Created by Alican SEKBAN on 18.01.2025.
+//
+
+import SwiftUI
+
+struct MuseumScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MuseumScreen()
+}
---
+++ MuseumsTurkey/screens/MuseumViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MuseumViewModel.swift
+//  MuseumsTurkey
+//
+//  Created by Alican SEKBAN on 18.01.2025.
+//
+
+import Foundation
