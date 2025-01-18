ec9cb28e5f02875e2ba9b80ac3d0fb8231b0afc1
Alican Sekban <alicansekban@hotmail.com>
2025-01-18 18:01:31 +0300

    Initial Commit

---
+++ MuseumsTurkey/MuseumsTurkeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseumsTurkeyApp.swift
+//  MuseumsTurkey
+//
+//  Created by Alican SEKBAN on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MuseumsTurkeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
