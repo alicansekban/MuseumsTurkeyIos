8cae6106742a922a1bfd8f474d52772832bc9d00
Alican Sekban <alicansekban@hotmail.com>
2025-01-18 18:16:48 +0300

    added bottom bar

--- MuseumsTurkey/ContentView.swift
+++ MuseumsTurkey/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            VStack {
+                
+            }
+            .background(Color.red)
+            .tabItem {
+                Image(systemName: "house")
+                Text("Müzeler")
+            }
+            
+            VStack {
+                
+            }
+            .background(Color.red)
+            .tabItem {
+                Image(systemName: "bookmark")
+                Text("Kaydedilenler")
+            }
+            VStack {
+                
+            }
+            .background(Color.red)
+            .tabItem {
+                Image(systemName: "person.circle")
+                Text("Hesabım")
+            }
         }
-        .padding()
     }
 }
 
