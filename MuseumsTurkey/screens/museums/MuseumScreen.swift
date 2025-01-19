59879432686f77e57a5b333e6ed35be434a0a078
Alican Sekban <alicansekban@hotmail.com>
2025-01-19 18:58:10 +0300

    added Museum Item

--- MuseumsTurkey/screens/museums/MuseumScreen.swift
+++ MuseumsTurkey/screens/museums/MuseumScreen.swift
@@ -17,18 +17,35 @@ struct MuseumScreen: View {
             ScrollView {
                 VStack(alignment: .leading) {
                     ForEach(viewModel.museums, id: \.self) { museum in
-                        Text(museum.name)
+                        MuseumItem(
+                            museumName: museum.name, museumAddress: museum.address, museumPhoneNumber: museum.phone
+                        )
                     }
-                    
+                
                 }
             }
             .onAppear {
-                
                 viewModel.fetchMuseums(forCity: "istanbul")
             }
         }
     }
 
+}
+
+struct MuseumItem : View {
+    @State var museumName: String = ""
+    @State var museumAddress : String = ""
+    @State var museumPhoneNumber : String = ""
+
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(museumName)
+                .bold()
+            Text(museumAddress)
+            Text(museumPhoneNumber)
+        }.padding(10)
+       }
 }
 
 #Preview {
